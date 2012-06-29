39d2caef4312832643138e9d1387ce930ba309e3
nhusted <nhusted@android-dev2.(none)>
2012-06-29 17:03:15 -0400

    Added the auditd binary target for compilation.

--- Android.mk
+++ Android.mk
@@ -28,3 +28,38 @@ LOCAL_CFLAGS :=  -fPIE -DPIE -g -D_GNU_SOURCE -fno-strict-aliasing
 LOCAL_LDLIBS := -lm -lpthread 
 
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+MY_ROOT_PATH := external/android_audit
+
+LOCAL_MODULE := auditd
+LOCAL_MODULE_TAGS := eng
+LOCAL_SRC_FILES:= lib/libaudit.c \
+			lib/message.c \
+			lib/netlink.c \
+			lib/lookup_table.c \
+			lib/audit_logging.c \
+			lib/deprecated.c \
+			src/auditd.c \
+			src/auditd-event.c \
+			src/auditd-config.c \
+			src/auditd-reconfig.c \
+			src/auditd-sendmail.c \
+			src/auditd-dispatch.c \
+			src/auditd-listen.c \
+			src/ev.c \
+			src/event.c
+
+
+LOCAL_C_INCLUDES :=	$(MY_ROOT_PATH) \
+			$(MY_ROOT_PATH)/lib \
+			$(MY_ROOT_PATH)/libc \
+			$(MY_ROOT_PATH)/kernel \
+			$(MY_ROOT_PATH)/src
+
+LOCAL_CFLAGS :=  -fPIE -DPIE -g -D_GNU_SOURCE -fno-strict-aliasing 
+
+LOCAL_LDLIBS := -lm -lpthread 
+
+include $(BUILD_EXECUTABLE)
