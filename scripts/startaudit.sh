a11ca91969f342c6fb8f4a966975c88caac280ed
nhusted <nhusted@indiana.edu>
2012-07-10 18:24:34 -0400

    Forgot to include the adb shells in front of a number of auditctl commands.

--- scripts/startaudit.sh
+++ scripts/startaudit.sh
@@ -15,16 +15,16 @@ adb shell auditctl -D
 auditdpids=$(adb shell ps | grep auditd | awk "{print \$2 }")
 for apid in $auditdpids
 do
-  auditctl -a exit,never -S all -F pid=$apid
+  adb shell auditctl -a exit,never -S all -F pid=$apid
 done
 
 # We need to find the adb process and ignore it
 adbpid=$(adb shell ps | grep adb | awk "{print \$2 }")
-auditctl -a exit,never -S all -F pid=$adbpid
+adb shell auditctl -a exit,never -S all -F pid=$adbpid
 
 #Double check our rules so far
-auditctl -l
+adb shell auditctl -l
 
 # Add our other rules
-auditctl -a exit,always -S clone -S execve -S exit_group -S open -S write -S mkdir -S mkdirat -S mknod -S chmod -S fchmod -S fchmodat -S chown -S fchown32 -S fchownat -S lchown32 -S close -S fork -S unlink -S link -S setuid -S symlink -S unlinkat -S setuid32 -S setreuid32 -S setresuid32
+adb shell auditctl -a exit,always -S clone -S execve -S exit_group -S open -S write -S mkdir -S mkdirat -S mknod -S chmod -S fchmod -S fchmodat -S chown -S fchown32 -S fchownat -S lchown32 -S close -S fork -S unlink -S link -S setuid -S symlink -S unlinkat -S setuid32 -S setreuid32 -S setresuid32
 
