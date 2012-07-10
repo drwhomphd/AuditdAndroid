3296a7b71837502c6366624ffd8e99494e06e500
nhusted <nhusted@indiana.edu>
2012-07-10 14:16:26 -0400

    Updated the License notices in a number of files now that other folks are looking at them.

--- libc/stpcpy.h
+++ libc/stpcpy.h
@@ -1,1 +1,24 @@
+/* stpcpy.h -- 
+ * Copyright 2004-2012 SRI International 
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors:
+ *  Nathaniel Husted <nhusted@gmail.com>
+ */
+
+/**
 char *stpcpy(register char * __restrict s1, const char * __restrict s2);
