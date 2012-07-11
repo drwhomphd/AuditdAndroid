a6080c8f4fbae065a84f21504539befd220d4a59
nhusted <nhusted@indiana.edu>
2012-07-11 18:58:59 -0400

    Added an initial application.mk to test builds with the Android NDK.

---
+++ Application.mk
@@ -0,0 +1,18 @@
+# The Application.mk is used with the NDK to get things to
+# build properly without using the default Google paths. Required
+# because forcing people to build in an arbitrary path structure
+# is stupid. I deny your ontology and replace it with my own.
+
+# Set the root project directory (where the Android.mk will be)
+APP_PROJECT_PATH := ./
+
+# Use the debug level of optimizations for now. You can uncomment the 
+# release line and comment the debug line if needed.
+APP_OPTIM := debug
+#APP_OPTIM := release
+
+# We have no CPP Flags, but just in case. These are also used as CFlags.
+APP_CPPFLAGS := 
+
+# We starget the x86 ABI for now
+APP_ABI := x86
