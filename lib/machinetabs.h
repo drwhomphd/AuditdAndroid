7b6256ff423515721cf8253a3138827ac1bd5d73
nhusted <nhusted@indiana.edu>
2012-07-30 16:39:15 -0400

    Updated machinetab file for arm platforms.

--- lib/machinetabs.h
+++ lib/machinetabs.h
@@ -1,10 +1,13 @@
 /* This is a generated file, see Makefile.am for its inputs. */
-static const char machine_strings[] = "i386\0i486\0i586\0i686\0ia64\0ppc\0ppc64\0s390\0s390x\0x86_64";
+static const char machine_strings[] = "armeb\0armv5tejl\0armv7l\0i386\0i486\0i586\0i686\0ia64\0ppc\0ppc64\0"
+	"s390\0s390x\0x86_64";
 static const unsigned machine_s2i_s[] = {
-	0,5,10,15,20,25,29,35,40,46,
+	0,6,16,23,28,33,38,43,48,52,
+	58,63,69,
 };
 static const int machine_s2i_i[] = {
-	0,0,0,0,2,4,3,6,5,1,
+	8,8,8,0,0,0,0,2,4,3,
+	6,5,1,
 };
 static int machine_s2i(const char *s, int *value) {
 	size_t len, i;
@@ -15,12 +18,12 @@ static int machine_s2i(const char *s, int *value) {
 		copy[i] = GT_ISUPPER(c) ? c - 'A' + 'a' : c;
 	}
 	copy[i] = 0;
-	return s2i__(machine_strings, machine_s2i_s, machine_s2i_i, 10, copy, value);
+	return s2i__(machine_strings, machine_s2i_s, machine_s2i_i, 13, copy, value);
 	}
 }
 static const unsigned machine_i2s_direct[] = {
-	0,46,20,29,25,40,35,
+	23,69,43,52,48,63,58,-1u,0,
 };
 static const char *machine_i2s(int v) {
-	return i2s_direct__(machine_strings, machine_i2s_direct, 0, 6, v);
+	return i2s_direct__(machine_strings, machine_i2s_direct, 0, 8, v);
 }
